5de5ba9831e9e7b548704bd4fd4fa6e58f6dcfb1
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-10-29 17:31:31 +0100

    fix task container

--- src/container/TaskContainer.js
+++ src/container/TaskContainer.js
@@ -4,7 +4,7 @@ import InputTask from "../features/task/InputTask";
 
 export default function TaskContainer() {
   return (
-    <div className="z-10 py-2 m-2 bg-black bg-opacity-70">
+    <div className="z-10 py-2 m-2 overflow-scroll bg-black bg-opacity-70 max-h-96">
       <InputTask></InputTask>
       <TaskList></TaskList>
     </div>
