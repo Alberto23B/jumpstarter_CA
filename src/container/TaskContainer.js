d92418fcfce2f7ecee9cc3bd2871a4ea12e1bc15
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-10-30 12:09:01 +0100

    minor Ui fix

--- src/container/TaskContainer.js
+++ src/container/TaskContainer.js
@@ -4,7 +4,7 @@ import InputTask from "../features/task/InputTask";
 
 export default function TaskContainer() {
   return (
-    <div className="py-2 m-2 overflow-auto bg-black bg-opacity-70 md:bg-opacity-100 max-h-96 md:max-h-[32rem] md:w-2/3 md:m-auto scroll">
+    <div className="py-2 m-2 overflow-auto bg-black bg-opacity-70 md:bg-opacity-100 max-h-96 md:max-h-[28rem] md:w-2/3 md:m-auto scroll">
       <InputTask></InputTask>
       <TaskList></TaskList>
     </div>
