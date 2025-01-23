64f8adcd4a3c4b3e1eda7022e6370b837390f933
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-23 22:09:55 +0100

    Create a StartedGame component

--- src/container/WeatherContainer.js
+++ src/container/WeatherContainer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import WeatherDisplay from "../features/weather/WeatherDisplay";
 import StartButtons from "../features/game/StartButtons";
+import StartedGame from "../features/game/StartedGame";
 
 export default function WeatherContainer() {
   return (
     <div className="z-10 flex w-4/5 m-auto">
       <WeatherDisplay />
-      <StartButtons />
+      {
+        // <StartButtons />
+      }
+      <StartedGame />
     </div>
   );
 }
