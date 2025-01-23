a70bfb0553d254fcda003fbfc81b6b32a2b02198
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-23 17:27:34 +0100

    Fix the form

--- src/features/task/InputTask.js
+++ src/features/task/InputTask.js
@@ -4,23 +4,6 @@ import { v4 as uuidv4 } from "uuid";
 import { useDispatch } from "react-redux";
 import { addTask } from "./tasksSlice";
 
-const star = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="inline size-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-    />
-  </svg>
-);
-
 export default function InputTask() {
   const [inputTask, setInputTask] = useState("");
   const dispatch = useDispatch();
@@ -47,7 +30,11 @@ export default function InputTask() {
 
   return (
     <div className="flex">
-      <form className="flex flex-col w-5/6 my-5 ml-2" onSubmit={handleSubmit}>
+      <form
+        className="flex flex-col w-5/6 my-5 ml-2"
+        onSubmit={handleSubmit}
+        id="input-task"
+      >
         <div className="w-full text-center">
           <input
             className="w-5/6 ml-2 text-black border-2 border-black"
@@ -74,7 +61,20 @@ export default function InputTask() {
           </svg>
           <p className="hidden md:inline">Estimated:</p>
           <input className="w-1/6 mr-4 text-black border-2 border-black" />
-          {star}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="inline size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+            />
+          </svg>
           <p className="hidden md:inline">Reward:</p>
           <select
             name="set-reward"
@@ -87,7 +87,13 @@ export default function InputTask() {
           </select>
         </div>
       </form>
-      <button className="m-auto text-4xl font-bold">&#xFF0B;</button>
+      <button
+        className="m-auto text-4xl font-bold"
+        type="submit"
+        form="input-task"
+      >
+        &#xFF0B;
+      </button>
     </div>
   );
 }
