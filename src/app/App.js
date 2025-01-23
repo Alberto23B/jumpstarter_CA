a31f5b9294e78da5994b62f5044f17915bf80551
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-23 15:24:09 +0100

    Remove quote feature

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "../container/Header";
 import ChangeBg from "../features/changeBg/ChangeBg";
 import WeatherContainer from "../container/WeatherContainer";
-import Quote from "../features/quote/Quote";
 import TaskContainer from "../container/TaskContainer";
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
         <main className="h-screen py-5 md:w-3/4 md:m-auto">
           <div className="relative z-10 flex justify-around py-5 m-2 bg-black md:m-auto md:w-2/3">
             <WeatherContainer />
-            <Quote />
           </div>
           <ChangeBg />
           <TaskContainer />
