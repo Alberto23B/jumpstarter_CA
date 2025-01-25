4b205d42de76d7910236c7f4f4dc6a44d627d646
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-25 11:11:06 +0100

    Add ability to startButton to set the difficulty

--- src/features/game/StartButtons.js
+++ src/features/game/StartButtons.js
@@ -1,6 +1,18 @@
 import ShowInfo from "../infos/ShowInfo";
+import { useDispatch } from "react-redux";
+import { setDifficulty } from "./gameSlice";
 
 export default function StartButtons() {
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    dispatch(
+      setDifficulty({
+        difficulty: e.target.value,
+      })
+    );
+  };
+
   return (
     <div className="flex flex-col justify-center w-full">
       <div className="w-4/5 m-auto">
@@ -8,13 +20,25 @@ export default function StartButtons() {
         <ShowInfo />
       </div>
       <div className="w-full text-center">
-        <button className="px-2 mx-1 text-sm border-2 border-white rounded-lg w-fit xl:w-1/4">
+        <button
+          className="px-2 mx-1 text-sm border-2 border-white rounded-lg w-fit xl:w-1/4"
+          value={"chill"}
+          onClick={handleClick}
+        >
           Chill 🟢
         </button>
-        <button className="px-1 mx-1 text-sm border-2 border-white rounded-lg w-fit xl:w-1/4">
+        <button
+          className="px-1 mx-1 text-sm border-2 border-white rounded-lg w-fit xl:w-1/4"
+          value={"motivated"}
+          onClick={handleClick}
+        >
           Motivated 🟡
         </button>
-        <button className="px-2 mx-1 text-sm border-2 border-white rounded-lg w-fit xl:w-1/4">
+        <button
+          className="px-2 mx-1 text-sm border-2 border-white rounded-lg w-fit xl:w-1/4"
+          value={"guru"}
+          onClick={handleClick}
+        >
           Guru 🔴
         </button>
       </div>
