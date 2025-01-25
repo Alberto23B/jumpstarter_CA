2641c871764d26cfdeb9fe6017c52999a9b1a3e8
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-25 15:43:12 +0100

    Add sorting functionality

--- src/container/TaskList.js
+++ src/container/TaskList.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import Task from "../features/task/Task";
 
 export default function TaskList() {
-  const tasks = useSelector((state) => state.tasks);
+  const isDifficultySelected = useSelector(
+    (state) => state.game.isDifficultySelected
+  );
+
+  const taskOrder = useSelector((state) => state.settings.order);
+
+  const userTasks = useSelector((state) => state.tasks);
+  // const gameTasks = useSelector((state) => state.game.tasks);
+
+  let sortedTasksRef = useRef(userTasks);
+
+  useEffect(() => {
+    let sorted = [...userTasks];
+    switch (taskOrder) {
+      case "estimated":
+        sorted.sort((a, b) => a.estimatedTime - b.estimatedTime);
+        break;
+      case "reward":
+        sorted.sort((a, b) => a.reward - b.reward);
+        break;
+      default:
+        sorted = userTasks;
+        break;
+    }
+
+    sortedTasksRef.current = sorted;
+  }, [taskOrder, userTasks]);
+
+  const tasks = isDifficultySelected ? sortedTasksRef.current || [] : userTasks;
 
   return (
     <div className="mx-2">
