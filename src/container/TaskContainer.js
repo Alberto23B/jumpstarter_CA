74fa10b00dde40560aca49f4b5cf884b5bf8a84f
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-27 15:54:27 +0100

    Fix taskContainer height on mobile

--- src/container/TaskContainer.js
+++ src/container/TaskContainer.js
@@ -5,7 +5,7 @@ import TaskStorageHandler from "../features/task/TaskStorageHandler";
 
 export default function TaskContainer() {
   return (
-    <div className="py-2 m-2 overflow-auto bg-black max-h-96 md:max-h-[28rem] md:w-2/3 md:m-auto scroll">
+    <div className="py-2 m-2 overflow-auto bg-black max-h-[32rem] md:max-h-[28rem] md:w-2/3 md:m-auto scroll">
       <InputTask></InputTask>
       <TaskList></TaskList>
       <TaskStorageHandler />
